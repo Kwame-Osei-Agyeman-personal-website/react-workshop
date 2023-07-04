19eaf37dfd969506449b16d88aad30f32d579e75
kwameagyeman <nana.agyeman2012@gmail.com>
2023-07-04 19:10:43 -0400

    feat:modified App.jsx file and created a new file called data.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
+import { useState } from "react";
+import { puppyList } from "./data";
+import "./App.css";
+
 function App() {
-  return <div></div>;
+  console.log(puppyList);
+
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  console.log("puppylist:", puppyList);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featPupId);
+
+  return (
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
+      })}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
